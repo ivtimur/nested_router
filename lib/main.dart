affec18c6be79d01faec837306b2a04daa2b7208
t.moiseev <t.moiseev@amiga.agency>
2023-06-26 00:01:33 +0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/main',
+    routes: [
+      StatefulShellRoute.indexedStack(
+        builder:
+            (BuildContext context, GoRouterState state, StatefulNavigationShell navigationShell) {
+          return ScaffoldNavBar(navigationShell: navigationShell);
+        },
+        branches: <StatefulShellBranch>[
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/main',
+                builder: (BuildContext context, GoRouterState state) => const MyHomePage(),
+                routes: <RouteBase>[
+                  GoRoute(
+                    path: 'details',
+                    builder: (BuildContext context, GoRouterState state) =>
+                        const DetailPage('from Main Page'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/tabB',
+                builder: (BuildContext context, GoRouterState state) => const TabB(),
+                routes: <RouteBase>[
+                  GoRoute(
+                    path: 'details',
+                    builder: (BuildContext context, GoRouterState state) =>
+                        const DetailPage('from Tab B'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      )
+    ],
+    redirect: (BuildContext context, state) {
+      return null;
+    },
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
+    );
+  }
+}
+
+class ScaffoldNavBar extends StatelessWidget {
+  const ScaffoldNavBar({
+    required this.navigationShell,
+    Key? key,
+  }) : super(key: key ?? const ValueKey<String>('ScaffoldNavBar'));
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Main'),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Tab b'),
+        ],
+        currentIndex: navigationShell.currentIndex,
+        onTap: (int index) => _onTap(context, index),
+      ),
+    );
+  }
+
+  void _onTap(BuildContext context, int index) {
+    navigationShell.goBranch(
+      index,
+      initialLocation: index == navigationShell.currentIndex,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, this.title = 'Home Page'});
 
   final String title;
 
@@ -61,6 +140,60 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  const DetailPage(
+    this.text, {
+    super.key,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page $text'),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: Text(text),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TabB extends StatelessWidget {
+  const TabB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TOP page of Tab B'),
+        actions: [IconButton(onPressed: () => context.go('/main'), icon: const Icon(Icons.home))],
+      ),
+      body: Scrollbar(
+        trackVisibility: true,
+        child: ListView.builder(
+          itemCount: 25,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () => context.go('/tabB/details'),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text('item: $index'),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
